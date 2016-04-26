2095467c1b4c32cb7504e61099a48c4f802bd53f
Annie <Annie@Annie-VAIO>
2016-04-26 08:16:22 -0400

    new commit

---
+++ src/java/user/data/AccountDB.java
@@ -0,0 +1,164 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package user.data;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import user.javabean.User;
+
+/**
+ *
+ * @author Annie
+ */
+public class AccountDB {
+
+    public static int insert(User user) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+
+        String query
+                = "INSERT INTO User (firstName, lastName, phone, address, city, "
+                + "state, zipCode, email, userName, userPassWord) "
+                + "VALUES (?, ?, ?)";
+
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, user.getFirstName());
+            ps.setString(2, user.getLastName());
+            ps.setString(3, user.getPhone());
+            ps.setString(4, user.getAddress());
+            ps.setString(5, user.getCity());
+            ps.setString(6, user.getState());
+            ps.setString(7, user.getZipCode());
+            ps.setString(8, user.getEmail());
+            ps.setString(9, user.getUserName());
+            ps.setString(10, user.getPassWord());
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+            return 0;
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+    }
+
+    public static int update(User user) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+
+        String query = "UPDATE User SET "
+                + "FirstName = ?, "
+                + "LastName = ? "
+                + "Phone = ? "
+                + "Address = ? "
+                + "City = ? "
+                + "State = ? "
+                + "ZipCode = ? "
+                + "Email = ? "
+                + "UserName = ? "
+                + "WHERE UserPassWord = ?";
+
+        try {
+            ps.setString(1, user.getFirstName());
+            ps.setString(2, user.getLastName());
+            ps.setString(3, user.getPhone());
+            ps.setString(4, user.getAddress());
+            ps.setString(5, user.getCity());
+            ps.setString(6, user.getState());
+            ps.setString(7, user.getZipCode());
+            ps.setString(8, user.getEmail());
+            ps.setString(9, user.getUserName());
+            ps.setString(10, user.getPassWord());
+
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+            return 0;
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+    }
+
+    public static int delete(User user) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+
+        String query = "DELETE FROM User "
+                + "WHERE UserName = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(9, user.getUserName());
+
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e);
+            return 0;
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+    }
+
+    public static boolean userNameExists(String userName) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String query = "SELECT UserName FROM User "
+                + "WHERE UserName = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(9, userName);
+            rs = ps.executeQuery();
+            return rs.next();
+        } catch (SQLException e) {
+            System.out.println(e);
+            return false;
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+    }
+
+    public static User selectUser(String userName) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        String query = "SELECT * FROM User "
+                + "WHERE UserName = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(9, userName);
+            rs = ps.executeQuery();
+            User user = null;
+            if (rs.next()) {
+                user = new User();
+                user.setFirstName(rs.getString("FirstName"));
+                user.setLastName(rs.getString("LastName"));
+                user.setEmail(rs.getString("Email"));
+            }
+            return user;
+        } catch (SQLException e) {
+            System.out.println(e);
+            return null;
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+    }
+}
